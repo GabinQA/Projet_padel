a8e5fe1f61c83ad4a91eefd749029405662f9a09
Bagzy3 <gabin.paolino@hotmail.com>
2025-04-06 17:36:40 +0200

    Premier commit de mon projet

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
-
-let lastScroll = 0;
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
-
-  if (currentScroll <= 0) {
-    header.style.transform = 'translateY(0)';
-    return;
-  }
-
-  if (currentScroll > lastScroll) {
-    // Scroll vers le bas → cacher la navbar
-    header.style.transform = 'translateY(-100%)';
-  } else {
-    // Scroll vers le haut → montrer la navbar
-    header.style.transform = 'translateY(0)';
-  }
-
-  lastScroll = currentScroll;
-});
